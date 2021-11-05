1716a082cb40428931caad363b4a0fd886d72397
Simran Sahajpal <simran.sahajpal1@gmail.com>
2021-11-05 15:51:35 +0100

    allows import of RectanglewObjects

--- ratsimulator/Environment/__init__.py
+++ ratsimulator/Environment/__init__.py
@@ -1,6 +1,7 @@
 from .ABCEnvironment import ABCEnvironment
 from .Rectangle import Rectangle
 from .OpenField import OpenField
-from .RectanglewObjects import *
+from .RectanglewObjects import RectanglewObjects
 
 from .methods import intersect, projection_rejection
+
