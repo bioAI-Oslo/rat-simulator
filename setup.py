815f164f2c0b07654bb776132b037d92a69ff237
vemundss <vemund@live.com>
2021-12-04 14:42:54 +0100

    increments version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rat-simulator",
-    version="1.1.0",
+    version="1.2.0",
     author="Vemund S. Schoyen",
     author_email="vemund@live.com",
     description="A rat in an environment simulator",
